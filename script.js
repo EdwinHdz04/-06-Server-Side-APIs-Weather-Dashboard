1310c86da584a37fa1667d6ddd522b73306dc58e
Edwin Hernandez <edwinhernandez80@icloud.com>
2023-01-11 21:48:19 -0800

    finished 5 day forcast function.

--- script.js
+++ script.js
@@ -8,14 +8,23 @@ var
 apiKey="46f48b1c2591f014d5caac1646b6d3ff"
 
 
+
 //This function will be populating the results of the weather
 function populateResults(result) {
     
-   console.log(result)
-   currentTemp.append("Temp: "+ result.temp + " &#176;F");
-   currentHumidity.append("Humidity: " + result.humidity + "%");
-   currentWind.append("Wind: " + result.wind + " MPH ");
-   resultCity.textContent = result.city;
+   console.log(result[0])
+   currentTemp.append("Temp: "+ result[0].temp + " &#176;F");
+   currentHumidity.append("Humidity: " + result[0].humidity + "%");
+   currentWind.append("Wind: " + result[0].wind + " MPH ");
+   resultCity.textContent = result[0].city;
+   
+   for (var i = 1; i <= 5; i++) {
+    $("#day"+i+" > .date").append(result[i].date.toString().substring(0,16))
+    $("#day"+i+" > .Temp").append("Temp: "+ result[i].temp + " &#176;F");
+    $("#day"+i+" > .wind").append("wind: "+ result[i].wind + " MPH ");
+    $("#day"+i+" > .Humidity").append("Humidity: "+ result[i].humidity + " % ");
+   } 
+
 
     
 }
@@ -28,11 +37,11 @@ function fetchlonlat(city){
     })
     .then(function(response) {
         
-        console.log(response)
+        
         var cityLon = response[0].lon;
         var cityLat = response[0].lat;
 
-        fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${cityLat}&lon=${cityLon}&appid=${apiKey}&units=imperial`)
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${cityLat}&lon=${cityLon}&appid=${apiKey}&units=imperial&cnt=40`)
            
             .then(function(response) {
                 
@@ -40,13 +49,44 @@ function fetchlonlat(city){
             })
       
             .then(function(response){
-                const result ={ 
-                     temp : response.list[0].main.temp,
-                     wind : response.list[0].wind.speed,
-                     humidity : response.list[0].main.humidity,
-                     city : response.city.name
+                
+                var result ={ 
+                0:{
+                     date : new Date(response.list[0].dt*1000),
+                    temp : response.list[0].main.temp,
+                    wind : response.list[0].wind.speed,
+                    humidity : response.list[0].main.humidity,},
+                1:{
+                     date : new Date(response.list[8].dt*1000),
+                    temp : response.list[8].main.temp,
+                    wind : response.list[8].wind.speed,
+                    humidity : response.list[8].main.humidity,},
+                2:{
+                     date : new Date(response.list[16].dt*1000),
+                    temp : response.list[16].main.temp,
+                    wind : response.list[16].wind.speed,
+                    humidity : response.list[16].main.humidity,},
+                3:{
+                     date : new Date(response.list[24].dt*1000),
+                    temp : response.list[24].main.temp,
+                    wind : response.list[24].wind.speed,
+                    humidity : response.list[24].main.humidity,},
+                4:{
+                     date : new Date(response.list[32].dt*1000),
+                temp : response.list[32].main.temp,
+                wind : response.list[32].wind.speed,
+                humidity : response.list[32].main.humidity,},
+                5:{
+                     date : new Date(response.list[39].dt*1000),
+                temp : response.list[39].main.temp,
+                wind : response.list[39].wind.speed,
+                humidity : response.list[39].main.humidity,},
+                
+                
                 }
                 
+                result.city = response.city.name
+             
                  populateResults(result);
 
                 
